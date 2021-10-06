03585fdd62d519fd979e5a33ce60a7d6cab7156c
daiki_matsumoto <64685202+daiki14@users.noreply.github.com>
2021-10-06 23:44:04 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* ==============================================================
+# スムーススクロール
+=========================================================== */
+jQuery('a[href^="#"]').on('click', function() {
+
+  var header = jQuery('.header').innerHeight();
+  var id = jQuery(this).attr('href');
+  var position = 0;
+  if ( id != '#' ) {
+    var position = jQuery(id).offset().top - header;
+  }
+  jQuery('html,body').animate({
+    scrollTop: position
+  },
+  300);
+});
